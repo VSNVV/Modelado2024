11eedb72219510d97d57a5a669a877b6a0934c74
viti <victor.sanavia@edu.uah.es>
2024-03-01 18:29:45 +0100

    Actualizaciones en display_controller.vhd
    He añadido la funcionalidad del Decodificador de hexadecimal a 7 segmentos, el display es de ánodo común, por lo que los leds se encenderán a un nivel bajo, o lo que es lo mismo, se encenderán con un 0

--- PracticaLibre/Apartado1/display_controller.vhd
+++ PracticaLibre/Apartado1/display_controller.vhd
@@ -16,9 +16,9 @@ architecture rtl of display_controller is
 signal RDOut : std_logic_vector(31 downto 0); -- Sennal de salida del Registro Desplazamiento y entrada al Multiplexor
 signal CntOut : std_logic_vector(2 downto 0); -- Sennal de salida del Contador y entrada al selector del Mux y al CC
 signal MuxOut : std_logic_vector(3 downto 0); -- Sennal de salida del Multiplexor
+signal DecOut : std_logic_vector(6 downto 0); -- Sennl de salida del Decodificador de hexadecimal a 7 segmentos
 
 begin
-
 
   process (RDOut, CntOut) -- Proceso del Mux o Multiplexor
   -- Declaracion de variables en el Mux
@@ -35,7 +35,7 @@ begin
     I7 := RDOut(31 downto 28);
     -- Una vez dividida la entrada en 4 bits, leemos el selector y asignamos la salida:
     case CntOut is
-      when "000" =>
+      when "000" => 
       -- El selector vale 0, por tanto, redirigmos a la salida el valor de I0
         MuxOut <= I0;
       when "001" =>
@@ -63,8 +63,43 @@ begin
     end case;
   end process;
 
-
-
-
-
+  process(MuxOut) -- Proceso del Decodificador en hexadecimal a 7 segmentos
+  begin
+    -- Simplemente tendremos que leer la señal de salida del Multiplexor y devolver el valor correspondiente a la salida
+    -- El display es de anodo comun, por tanto, los leds se encienden con un nivel bajo, o lo que es lo mismo, con un 0
+    case MuxOut is
+      when x"1" =>
+        DecOut <= "1111001"; -- Numero 1
+      when x"2" =>
+        DecOut <= "0100100"; -- Numero 2
+      when x"3" =>
+        DecOut <= "0110000"; -- Numero 3
+      when x"4" =>
+        DecOut <= "0011001"; -- Numero 4
+      when x"5" =>
+        DecOut <= "0010010"; -- Numero 5
+      when x"6" =>
+        DecOut <= "0000010"; -- Numero 6
+      when x"7" =>
+        DecOut <= "1111000"; -- Numero 7
+      when x"8" =>
+        DecOut <= "0000000"; -- Numero 8
+      when x"9" =>
+        DecOut <= "0011000"; -- Numero 9
+      when x"A" =>
+        DecOut <= "0001000"; -- Letra A
+      when x"B" =>
+        DecOut <= "0000011"; -- Letra B
+      when x"C" =>
+        DecOut <= "1000110"; -- Letra C
+      when x"D" =>
+        DecOut <= "0100001"; -- Letra D
+      when x"E" =>
+        DecOut <= "0000110"; -- Letra E
+      when x"F" =>
+        DecOut <= "0001110"; -- Letra F
+      when others =>
+        DecOut <= "1111111"; -- Display Apagado
+    end case;
+  end process;
 end;
