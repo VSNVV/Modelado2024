edaa5d1e16de1542ff649354068fd8be7a284965
viti <victor.sanavia@edu.uah.es>
2024-03-18 14:32:58 +0100

    Mi código también fufa jiji

--- P1/Source/display_controller.vhd
+++ P1/Source/display_controller.vhd
@@ -2,8 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 entity display_controller is
-  port(
-       RST        : in  std_logic;
+  port(RST        : in  std_logic;
        CLK        : in  std_logic;
        DATO_RX_OK : in  std_logic;
        DATO_RX    : in  std_logic_vector(7 downto 0);
@@ -13,198 +12,191 @@ entity display_controller is
 end display_controller;
 
 architecture rtl of display_controller is
+-- Declaracion de sennales
+-- constant CTE_ANDS      : integer := 100000;   -- para la implementacion
+constant CTE_ANDS  : integer := 200;  -- para la simulacion
+signal CntReg : integer range 0 to CTE_ANDS - 1;
+signal RDOut : std_logic_vector(31 downto 0); -- Sennal de salida del Registro de Desplazamiento
+signal PROut : std_logic; -- Sennal de salida del Prescaler
+signal CntOut : unsigned(2 downto 0); -- Sennal de salida del Contador
+signal MuxOut : std_logic_vector(3 downto 0); -- Sennal de salida del Multiplexor
+signal DecHexOut : std_logic_vector(6 downto 0); -- Sennal de salida del decodificador hexadecimal
+signal CCOut : std_logic; -- Sennal de salida del Circuito Combinacional
+signal Dec3To8Out : std_logic_vector(7 downto 0); -- Sennal de salida del Decodificador de 3 a 8
 
-  --constant CTE_ANDS      : integer := 100000;   -- para la implementación
-  constant CTE_ANDS      : integer := 200;  -- para la simulación
-  signal   counter_reg : integer range 0 to CTE_ANDS-1;
-  signal   prescaler_out :std_logic;
-  signal   reg_desp: std_logic_vector(31 downto 0);
-  signal   cont_out: unsigned (2 downto 0);
-  signal   multiplexor_out: std_logic_vector(3 downto 0);
-  signal   dec_hexTo7_out: std_logic_vector(6 downto 0);
-  signal   cc_out: std_logic;
-  signal   dec_3To8_out :std_logic_vector(7 downto 0);
-    
-begin  
-
-prescaler:  process (CLK, RST)
-    begin  -- process
-        if rst = '1' then
-            counter_reg   <= 0;
-        elsif clk'event and clk = '1' then
-            if counter_reg = CTE_ANDS-1 then
-                counter_reg <= 0;
-            else
-            counter_reg <= counter_reg+1;
-            end if;
-        end if;
-    end process;
-
-cont_presscaler:  process (CLK, RST)
-    begin  -- process
-        if rst = '1' then
-            prescaler_out   <= '0';
-        elsif clk'event and clk = '1' then
-            if counter_reg = CTE_ANDS-1 then
-                prescaler_out <= '1';
-            else
-                prescaler_out <= '0';
-            end if;
-        end if;
-    end process;
-
-registro_desp: process(CLK, RST) --registro desplazamiento
-    begin
-        if(rst = '1') then
-            reg_desp <= (others => '0');
-        elsif(clk'event and clk = '1') then if(dato_rx_ok = '1') then
-            reg_desp<= reg_desp(23 downto 0)& dato_rx;
-            end if;
-        end if;
-    end process;
- 
-contador0a7 :process(prescaler_out,CLK,RST)
-    begin
-        if rst = '1' then
-            cont_out   <= "000";
-        elsif clk'event and clk = '1' then
-            if cont_out = "111" then
-                cont_out<="000";
-            else
-                cont_out<=cont_out+1;
-            end if;
-        end if;
-    end process;
-       
- 
-multiplexor8a1 : process(cont_out,reg_desp)
-    begin
-        case cont_out is
-            when "000" => 
-                multiplexor_out <=reg_desp(3 downto 0);
-            when "001" => 
-                multiplexor_out <=reg_desp(7 downto 4);
-            when "010" => 
-                multiplexor_out <=reg_desp(11 downto 8);
-            when "011" => 
-                multiplexor_out <=reg_desp(15 downto 12);
-            when "100" =>   
-                multiplexor_out <=reg_desp(19 downto 16);
-            when "101" => 
-                multiplexor_out <=reg_desp(23 downto 20);
-            when "110" => 
-                multiplexor_out <=reg_desp(27 downto 24);
-            when "111" => 
-                multiplexor_out <=reg_desp(31 downto 28);
-            when others => 
-            multiplexor_out <=reg_desp(31 downto 28);
-        end case;
-    end process;
-
-decodificadorHexTo7: process(multiplexor_out)
-    begin  
-        case multiplexor_out is
-           when x"0" =>
-                dec_hexTo7_out <= "0000001";
-            when x"1" =>
-                dec_hexTo7_out <= "1001111";
-            when x"2" =>
-                dec_hexTo7_out <= "0010010";
-            when x"3" =>
-                dec_hexTo7_out <= "0000110";
-            when x"4" =>
-                dec_hexTo7_out <= "1001100";
-            when x"5" =>
-                dec_hexTo7_out <= "0100100";
-            when x"6" =>
-                dec_hexTo7_out <= "0100000";
-            when x"7" =>
-                dec_hexTo7_out <= "0001111";
-            when x"8" =>
-                dec_hexTo7_out <= "0000000";
-            when x"9" =>
-                dec_hexTo7_out <= "0000100";
-            when x"A" =>
-                dec_hexTo7_out <= "0001000";
-            when x"B" =>
-                dec_hexTo7_out <= "1100000";
-            when x"C" =>
-                dec_hexTo7_out <= "0110001";
-            when x"D" =>
-                dec_hexTo7_out <= "1000010";
-            when x"E" =>
-                dec_hexTo7_out <= "0110000";
-            when x"F" =>
-                dec_hexTo7_out <= "0111000";
-            when others =>
-                dec_hexTo7_out <= "1111111";
-        end case;
-    end process;
-decodificador3a8:process(cont_out)
-    begin
-        case cont_out is
-            when "000"=>
-                dec_3To8_out <= "11111110";
-            when "001"=>
-                dec_3To8_out <= "11111101";
-            when "010"=>
-                dec_3To8_out <= "11111011";
-            when "011"=>
-                dec_3To8_out <= "11110111";
-            when "100"=>
-                dec_3To8_out <= "11101111";
-            when "101"=>
-                dec_3To8_out <= "11011111";
-            when "110"=>
-                dec_3To8_out <= "10111111";
-            when "111"=>
-                dec_3To8_out <= "01111111";
-            when others =>
-                dec_3To8_out <= "11111111"; 
-        end case;
-end process;
-
-
-
-
-process (CLK, RST) --Register SEG_AG
-    begin
-    if RST = '1' then
-        SEG_AG <= "0000000";
-    elsif CLK'event and CLK = '1' then
-        SEG_AG <= dec_hexTo7_out; 
+begin
+  process(DATO_RX, DATO_RX_OK, CLK, RST) -- Proceso del Registro de Desplazamiento
+  begin
+    if clk'event and clk = '1' then
+      if DATO_RX_OK = '1' then
+        RDOut <= RDOut(23 downto 0) & DATO_RX;
+      end if;
+    elsif RST = '1' then
+      RDOut <= (others => '0');
     end if;
-end process;
+  end process;
 
-
-process (CLK, RST) --Register DP
-    begin
+  process(CLK, RST) -- Proceso del Prescaler
+  begin
     if RST = '1' then
-        DP <= '0';
-    elsif CLK'event and CLK = '1' then
-        DP <= CC_out; 
+      CntReg <= 0;
+    elsif clk'event and clk ='1' then
+      if CntReg = CTE_ANDS - 1 then
+        CntReg <= 0;
+      else
+        CntReg <= CntReg + 1;
+      end if;
     end if;
-end process;
+  end process;
 
-
-process (CLK, RST) --Register AND_70
-    begin
+  process (CLK, RST) -- Proceso del Contador del Prescaler
+  begin
     if RST = '1' then
-        AND_70 <= "00000000";
+      PROut <= '0';
     elsif CLK'event and CLK = '1' then
-        AND_70 <= dec_3To8_out; 
+      if CntReg = CTE_ANDS - 1 then
+        PROut <= '1';
+      else
+        PROut <= '0';
+      end if;
     end if;
-end process;
+  end process;
 
---cc_out<= '1' when cont_out rem 2=0 else'0';
-circuitoCombinacional:process(cont_out)
-    begin
-        if cont_out rem 2= 0 then
-            cc_out<='1';
+  process(PROut, CLK, RST) -- Proceso del Contador
+  begin
+    if RST = '1' then
+      CntOut <= "000";
+    elsif CLK'event and CLK = '1' then
+      if PROut = '1' then
+        if CntOut = "111" then
+          CntOut <= "000";
         else
-            cc_out<='0';
+          CntOut <= CntOut + 1;
         end if;
-    end process;
+      end if;
+    end if;
+  end process;
+
+  process(RDOut, CntOut) -- Proceso del Multiplexor
+  begin
+    case CntOut is
+      when "000" =>
+        MuxOut <= RDOut(3 downto 0);
+      when "001" =>
+        MuxOut <= RDOut(7 downto 4);
+      when "010" =>
+        MuxOut <= RDOut(11 downto 8);
+      when "011" =>
+        MuxOut <= RDOut(15 downto 12);
+      when "100" =>
+        MuxOut <= RDOut(19 downto 16);
+      when "101" =>
+        MuxOut <= RDOut(23 downto 20);
+      when "110" =>
+        MuxOut <= RDOut(27 downto 24);
+      when "111" =>
+        MuxOut <= RDOut(31 downto 28);
+      when others =>
+        MuxOut <= RDOut(31 downto 28);
+    end case;
+  end process;
+
+  process(MuxOut) -- Proceso del Decodificador de Hexadecimal a 7 segmentos
+  begin
+    case MuxOut is
+      when x"1" =>
+        DecHexOut <= "0000001"; -- Numero 1
+      when x"2" =>
+        DecHexOut <= "1001111"; -- Numero 2
+      when x"3" =>
+        DecHexOut <= "0010010"; -- Numero 3
+      when x"4" =>
+        DecHexOut <= "1001100"; -- Numero 4
+      when x"5" =>
+        DecHexOut <= "0100100"; -- Numero 5
+      when x"6" =>
+        DecHexOut <= "0100000"; -- Numero 6
+      when x"7" =>
+        DecHexOut <= "0001111"; -- Numero 7
+      when x"8" =>
+        DecHexOut <= "0000000"; -- Numero 8
+      when x"9" =>
+        DecHexOut <= "0000100"; -- Numero 9
+      when x"A" =>
+        DecHexOut <= "0001000"; -- Letra A
+      when x"B" =>
+        DecHexOut <= "1100000"; -- Letra B
+      when x"C" =>
+        DecHexOut <= "0110001"; -- Letra C
+      when x"D" =>
+        DecHexOut <= "1000010"; -- Letra D
+      when x"E" =>
+        DecHexOut <= "0110000"; -- Letra E
+      when x"F" =>
+        DecHexOut <= "0111000"; -- Letra F
+      when others =>
+        DecHexOut <= "1111111"; -- Display Apagado
+    end case;
+  end process;
+
+  process(DecHexOut, CLK, RST) -- Proceso del Registro SEG_AG (Biestable tipo D)
+  begin
+    if RST = '1' then
+      SEG_AG <= "0000000"; -- Como es anodo comun, estarian todos los leds apagados
+    elsif clk'event and clk = '1' then
+      SEG_AG <= DecHexOut;
+    end if;
+  end process;
+
+  process(CntOut) -- Proceso del Circuito Combinacional
+  begin
+    if CntOut rem 2 = 0 then
+      CCOut <= '1';
+    else
+      CCOut <= '0';
+    end if;
+  end process;
+
+  process(CCOut, CLK, RST) -- Proceos del Registro DP (Biestable tipo D)
+  begin
+    if RST = '1' then
+      DP <= '0';
+    elsif CLK'event and CLK = '1' then
+      DP <= CCOut;
+    end if;
+  end process;
+
+  process(CntOut) -- Proceso del Decodificador de 3 a 8
+  begin
+    case CntOut is
+      when "000" =>
+        Dec3To8Out <= "11111110";
+      when "001" =>
+        Dec3To8Out <= "11111101";
+      when "010" =>
+        Dec3To8Out <= "11111011";
+      when "011" =>
+        Dec3To8Out <= "11110111";
+      when "100" =>
+        Dec3To8Out <= "11101111";
+      when "101" =>
+        Dec3To8Out <= "11011111";
+      when "110" =>
+        Dec3To8Out <= "10111111";
+      when "111" =>
+        Dec3To8Out <= "01111111";
+      when others =>
+        Dec3To8Out <= "11111111";
+    end case;
+  end process;
+
+  process(Dec3To8Out, CLK, RST) -- Proceso del Registro AND_70 (Biestable tipo D)
+  begin
+    if RST = '1' then
+      AND_70 <= "00000000";
+    elsif CLK'event and CLK = '1' then
+      AND_70 <= Dec3To8Out;
+    end if;
+  end process;
+
 end rtl;
-
-
--- PracticaLibre/Apartado1/display_controller.vhd
+++ PracticaLibre/Apartado1/display_controller.vhd
@@ -12,94 +12,189 @@ entity display_controller is
 end display_controller;
 
 architecture rtl of display_controller is
---Declaracion de sennales
-signal RDOut : std_logic_vector(31 downto 0); -- Sennal de salida del Registro Desplazamiento y entrada al Multiplexor
-signal CntOut : std_logic_vector(2 downto 0); -- Sennal de salida del Contador y entrada al selector del Mux y al CC
+-- Declaracion de sennales
+-- constant CTE_ANDS      : integer := 100000;   -- para la implementacion
+constant CTE_ANDS  : integer := 200;  -- para la simulacion
+signal CntReg : integer range 0 to CTE_ANDS - 1;
+signal RDOut : std_logic_vector(31 downto 0); -- Sennal de salida del Registro de Desplazamiento
+signal PROut : std_logic; -- Sennal de salida del Prescaler
+signal CntOut : unsigned(2 downto 0); -- Sennal de salida del Contador
 signal MuxOut : std_logic_vector(3 downto 0); -- Sennal de salida del Multiplexor
-signal DecOut : std_logic_vector(6 downto 0); -- Sennl de salida del Decodificador de hexadecimal a 7 segmentos
+signal DecHexOut : std_logic_vector(6 downto 0); -- Sennal de salida del decodificador hexadecimal
+signal CCOut : std_logic; -- Sennal de salida del Circuito Combinacional
+signal Dec3To8Out = std_logic_vector(7 downto 0); -- Sennal de salida del Decodificador de 3 a 8
 
 begin
-
-  process (RDOut, CntOut) -- Proceso del Mux o Multiplexor
-  -- Declaracion de variables en el Mux
-  variable I0, I1, I2, I3, I4, I5, I6, I7 : std_logic_vector(3 downto 0);
+  process(DATO_RX, DATO_RX_OK, CLK, RST) -- Proceso del Registro de Desplazamiento
   begin
-    -- Dividmos la sennal RDOut entre los inputs del Muktiplexor, cada uno de 4 bits
-    I0 := RDOut(3 downto 0);
-    I1 := RDOut(7 downto 4);
-    I2 := RDOut(11 downto 8);
-    I3 := RDOut(15 downto 12);
-    I4 := RDOut(19 downto 16);
-    I5 := RDOut(23 downto 20);
-    I6 := RDOut(27 downto 24);
-    I7 := RDOut(31 downto 28);
-    -- Una vez dividida la entrada en 4 bits, leemos el selector y asignamos la salida:
+    if clk'event and clk = '1' then
+      if DATO_RX_OK = '1' then
+        RDOut <= RDOut(23 downto 0) & DATO_RX
+      end if;
+    elsif RST = '1' then
+      RDOut <= others => '0';
+    end if;
+  end process;
+
+  process(CLK, RST) -- Proceso del Prescaler
+  begin
+    if RST = '1' then
+      CntReg <= 0;
+    elsif clk'event and clk ='1' then
+      if CntReg = CLKDIV - 1 then
+        CntReg <= 0;
+      else
+        CntReg <= CntReg + 1;
+      end if;
+    end if;
+  end process;
+
+  process (CLK, RST) -- Proceso del Contador del Prescaler
+  begin
+    if RST = '1' then
+      PROut <= '0';
+    elsif CLK'event and CLK = '1' then
+      if CntReg = CTE_ANDS - 1 then
+        PROut <= '1';
+      else
+        PROut <= '0';
+      end if;
+    end if;
+  end process;
+
+  process(PROut, CLK, RST) -- Proceso del Contador
+  begin
+    if RST = '1' then
+      CntOut <= "000";
+    elsif CLK'event and CLK = '1' then
+      if PROut = '1' then
+        if CntOut = "111" then
+          CntOut <= "000";
+        else
+          CntOut <= CntOut + 1;
+        end if;
+      end if;
+    end if;
+  end process;
+
+  process(RDOut, CntOut) -- Proceso del Multiplexor
+  begin
     case CntOut is
-      when "000" => 
-      -- El selector vale 0, por tanto, redirigmos a la salida el valor de I0
-        MuxOut <= I0;
-      when "001" =>
-      -- El selector vale 1, por tanto, redirigmos a la salida el valor de I0
-        MuxOut <= I1;
-      when "010" =>
-      -- El selector vale 2, por tanto, redirigmos a la salida el valor de I0
-        MuxOut <= I2;
-      when "011" =>
-      -- El selector vale 3, por tanto, redirigmos a la salida el valor de I0
-        MuxOut <= I3;
-      when "100" =>
-      -- El selector vale 4, por tanto, redirigmos a la salida el valor de I0
-        MuxOut <= I4;
-      when "101" =>
-      -- El selector vale 5, por tanto, redirigmos a la salida el valor de I0
-        MuxOut <= I5;
-      when "110" =>
-      -- El selector vale 6, por tanto, redirigmos a la salida el valor de I0
-        MuxOut <= I6;
-      when "111" =>
-      -- El selector vale 7, por tanto, redirigmos a la salida el valor de I0
-        MuxOut <= I7;
-      when others => MuxOut <= 0;
+      when "000" then =>
+        MuxOut <= RDOut(3 downto 0);
+      when "001" then =>
+        MuxOut <= RDOut(7 downto 4);
+      when "010" then =>
+        MuxOut <= RDOut(11 downto 8);
+      when "011" then =>
+        MuxOut <= RDOut(15 downto 12);
+      when "100" then =>
+        MuxOut <= RDOut(19 downto 16);
+      when "101" then =>
+        MuxOut <= RDOut(23 downto 20);
+      when "110" then =>
+        MuxOut <= RDOut(27 downto 24);
+      when "111" then =>
+        MuxOut <= RDOut(31 downto 28);
     end case;
   end process;
 
-  process(MuxOut) -- Proceso del Decodificador en hexadecimal a 7 segmentos
+  process(MuxOut) -- Proceso del Decodificador de Hexadecimal a 7 segmentos
   begin
-    -- Simplemente tendremos que leer la señal de salida del Multiplexor y devolver el valor correspondiente a la salida
-    -- El display es de anodo comun, por tanto, los leds se encienden con un nivel bajo, o lo que es lo mismo, con un 0
     case MuxOut is
       when x"1" =>
-        DecOut <= "1111001"; -- Numero 1
+        DecHexOut <= "0000001"; -- Numero 1
       when x"2" =>
-        DecOut <= "0100100"; -- Numero 2
+        DecHexOut <= "1001111"; -- Numero 2
       when x"3" =>
-        DecOut <= "0110000"; -- Numero 3
+        DecHexOut <= "0010010"; -- Numero 3
       when x"4" =>
-        DecOut <= "0011001"; -- Numero 4
+        DecHexOut <= "1001100"; -- Numero 4
       when x"5" =>
-        DecOut <= "0010010"; -- Numero 5
+        DecHexOut <= "0100100"; -- Numero 5
       when x"6" =>
-        DecOut <= "0000010"; -- Numero 6
+        DecHexOut <= "0100000"; -- Numero 6
       when x"7" =>
-        DecOut <= "1111000"; -- Numero 7
+        DecHexOut <= "0001111"; -- Numero 7
       when x"8" =>
-        DecOut <= "0000000"; -- Numero 8
+        DecHexOut <= "0000000"; -- Numero 8
       when x"9" =>
-        DecOut <= "0011000"; -- Numero 9
+        DecHexOut <= "0000100"; -- Numero 9
       when x"A" =>
-        DecOut <= "0001000"; -- Letra A
+        DecHexOut <= "0001000"; -- Letra A
       when x"B" =>
-        DecOut <= "0000011"; -- Letra B
+        DecHexOut <= "1100000"; -- Letra B
       when x"C" =>
-        DecOut <= "1000110"; -- Letra C
+        DecHexOut <= "0110001"; -- Letra C
       when x"D" =>
-        DecOut <= "0100001"; -- Letra D
+        DecHexOut <= "10000010"; -- Letra D
       when x"E" =>
-        DecOut <= "0000110"; -- Letra E
+        DecHexOut <= "0110000"; -- Letra E
       when x"F" =>
-        DecOut <= "0001110"; -- Letra F
+        DecHexOut <= "0111000"; -- Letra F
       when others =>
-        DecOut <= "1111111"; -- Display Apagado
+        DecHexOut <= "1111111"; -- Display Apagado
     end case;
   end process;
-end;
+
+  process(DecOut, CLK, RST) -- Proceso del Registro SEG_AG (Biestable tipo D)
+  begin
+    if RST = '1' then
+      SEG_AG = "0000000" -- Como es ánodo común, estarían todos los leds apagados
+    elsif clk'event and clk = '1' then
+      SEG_AG <= DecOut;
+    end if;
+  end process;
+
+  process(CntOut) -- Proceso del Circuito Combinacional
+  begin
+    if CntOut rem 2 = 0 then
+      CCOut <= '1';
+    else
+      CCOut <= '0';
+    end if;
+  end process;
+
+  process(CCOut, CLK, RST) -- Proceos del Registro DP (Biestable tipo D)
+  begin
+    if RST = '1' then
+      DP <= '0';
+    elsif CLK'event and CLK = '1' then
+      DP <= CCOut;
+    end if;
+  end process;
+
+  process(CntOut) -- Proceso del Decodificador de 3 a 8
+  begin
+    case CntOut is
+      when "000" =>
+        Dec3To8Out <= "11111110";
+      when "001" =>
+        Dec3To8Out <= "11111101";
+      when "010" =>
+        Dec3To8Out <= "11111011";
+      when "011" =>
+        Dec3To8Out <= "11110111";
+      when "100" =>
+        Dec3To8Out <= "11101111";
+      when "101" =>
+        Dec3To8Out <= "11011111";
+      when "110" =>
+        Dec3To8Out <= "10111111";
+      when "111" =>
+        Dec3To8Out <= "01111111";
+      when others =>
+        Dec3To8Out <= "11111111";
+    end case;
+  end process;
+
+  process(Dec3To8Out, CLK, RST) -- Proceso del Registro AND_70 (Biestable tipo D)
+  begin
+    if RST = '1' then
+      AND_70 <= "00000000"
+    elsif CLK'event and CLK = '1' then
+      AND_70 <= Dec3To8Out;
+    end if;
+  end process;
+
+end rtl;
