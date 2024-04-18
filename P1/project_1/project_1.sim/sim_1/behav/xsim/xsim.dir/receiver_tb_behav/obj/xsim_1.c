e4e4d1fdc09af821cfcbea1c9f9399e6eb3c994e
XxmazafesioxX <iker.valdezate@edu.uah.es>
2024-04-18 08:32:30 +0200

    cumit

--- P1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/receiver_tb_behav/obj/xsim_1.c
+++ P1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/receiver_tb_behav/obj/xsim_1.c
@@ -44,8 +44,9 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
+extern void execute_38(char*, char *);
 extern void execute_39(char*, char *);
-extern void execute_40(char*, char *);
+extern void execute_24(char*, char *);
 extern void execute_25(char*, char *);
 extern void execute_26(char*, char *);
 extern void execute_27(char*, char *);
@@ -54,20 +55,18 @@ extern void execute_29(char*, char *);
 extern void execute_30(char*, char *);
 extern void execute_31(char*, char *);
 extern void execute_32(char*, char *);
-extern void execute_33(char*, char *);
-extern void execute_34(char*, char *);
+extern void execute_35(char*, char *);
 extern void execute_36(char*, char *);
-extern void execute_37(char*, char *);
 extern void transaction_0(char*, char*, unsigned, unsigned, unsigned);
 extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[16] = {(funcp)execute_39, (funcp)execute_40, (funcp)execute_25, (funcp)execute_26, (funcp)execute_27, (funcp)execute_28, (funcp)execute_29, (funcp)execute_30, (funcp)execute_31, (funcp)execute_32, (funcp)execute_33, (funcp)execute_34, (funcp)execute_36, (funcp)execute_37, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
-const int NumRelocateId= 16;
+funcp funcTab[15] = {(funcp)execute_38, (funcp)execute_39, (funcp)execute_24, (funcp)execute_25, (funcp)execute_26, (funcp)execute_27, (funcp)execute_28, (funcp)execute_29, (funcp)execute_30, (funcp)execute_31, (funcp)execute_32, (funcp)execute_35, (funcp)execute_36, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
+const int NumRelocateId= 15;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/receiver_tb_behav/xsim.reloc",  (void **)funcTab, 16);
-	iki_vhdl_file_variable_register(dp + 6632);
-	iki_vhdl_file_variable_register(dp + 6688);
+	iki_relocate(dp, "xsim.dir/receiver_tb_behav/xsim.reloc",  (void **)funcTab, 15);
+	iki_vhdl_file_variable_register(dp + 6200);
+	iki_vhdl_file_variable_register(dp + 6256);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
--- PracticaLibre/Apartado3/receiver.vhd
+++ PracticaLibre/Apartado3/receiver.vhd
@@ -16,15 +16,19 @@ end receiver;
 architecture rtl of receiver is
 -- Declaracion de las Sennales y Constantes
 constant tiempoMuestreo : integer := 29; -- Constante del prescaler, 4.34us --> 4340ns --> 4340 / 15 = 289.33 --> 289.33 / 10 = 28.93 --> 29 CLK
+constant numeroMuestras : integer := 15; -- Constante del numero de muestras que se hacen para cada bit
+constant numeroBits : integer := 11; -- Constante del numero de bits que se reciven
 --constant tiempoMuestreo : integer := 31; -- Constante del prescaler, 4.34us --> 4340ns --> 4340 / 14 = 310 --> 310 / 10 = 31 --> 31 CLK
-signal PR_Cnt : unsigned(3 downto 0); -- Sennal del contador del Prescaler
+signal PR_Cnt : unsigned(4 downto 0); -- Sennal del contador del Prescaler
 signal PR_FC : std_logic; -- Sennal de salida del Prescaler
-signal FSM_PR : std_logic; -- Sennal que manda la FSM al Prescaler para que este siga contando o no, en caso de que no, se reincia a 0
-signal FSM_CntM : std_logic; -- Sennal que manda la FSM al contador de muestras para que pueda contar o no
-signal FSM_CntB : std_logic; -- Sennal que manda la FSM al contador de bits para que pueda contar o no
-signal FSM_Val : std_logic; -- Sennal que manda la FSM al Registro para enviar el dato por dato_rx
-signal CntM_Out : unsigned(3 downto 0); -- Sennal de salida del Contador que cuenta las MUESTRAS realizadas sobre un bit
-signal CntB_Out : unsigned(3 downto 0); -- Sennal de salida del COntador que cuenta los BITS leidos
+--signal FSM_PR : std_logic; -- Sennal que manda la FSM al Prescaler para que este siga contando o no, en caso de que no, se reincia a 0
+--signal FSM_CntM : std_logic; -- Sennal que manda la FSM al contador de muestras para que pueda contar o no
+--signal FSM_CntB : std_logic; -- Sennal que manda la FSM al contador de bits para que pueda contar o no
+--signal FSM_Val : std_logic; -- Sennal que manda la FSM al Registro para enviar el dato por dato_rx
+signal CntM_Cnt : unsigned(3 downto 0); -- Sennal de salida del Contador que cuenta las MUESTRAS realizadas sobre un bit
+signal CntM_FC : std_logic;
+signal CntB_Cnt : unsigned(3 downto 0); -- Sennal de salida del COntador que cuenta los BITS leidos
+signal CntB_FC : std_logic;
 signal RDM_Out : std_logic_vector(14 downto 0); -- Sennal de salida del Registro de Desplazamiento que sobremuestrea RX
 signal RDB_Out : std_logic_vector(10 downto 0); -- Sennal de salida del Registro de Desplazamiento que contiene los bits finales
 signal CntBin : std_logic; -- Sennal de salida del Circuito Combinacional que cuenta el numero de 1 y 0 de las muestras de RX
@@ -35,76 +39,145 @@ signal STD_Act: FSM; --estado actual de la maquina de estados
 
 begin
 
-  process(FSM_PR, CLK, RST) -- Proceso que modela el Prescaler
+--  process(STD_Act, FSM_PR, CLK, RST) -- Proceso que modela el Prescaler
+--  begin
+--    if RST = '1' then
+--      PR_Cnt <= (others => '0');
+--      PR_FC <= '0';
+--    elsif CLK'event and CLK = '1' then
+--      -- Ahora tenemos que ver si podemos seguir contando
+--        -- Significa que podemos contar, aunque si estamos en 27, tendremos que reiniciar
+--        if PR_Cnt = tiempoMuestreo - 1 then
+--          -- Se verifica que se ha llegado al fin de cuenta, y que tenemos que reiniciar
+--          PR_Cnt <= (others => '0');
+--          PR_FC <= '1';
+--        else
+--          -- No estamos a fin de cuenta, por tanto tenemos que incrementar el contador
+--          PR_Cnt <= PR_Cnt + 1;
+--          PR_FC <= '0';
+--        end if;
+--      else
+--        -- Verificamos que no podemos contar porque la maquina de estados no esta recibiendo, por tanto reiniciamos
+--        PR_Cnt <= (others => '0');
+--        PR_FC <= '0';
+--      end if;
+--    end if;
+--  end process;
+  
+  process(STD_Act, CLK, RST) -- Proceso que modela el Prescaler
   begin
     if RST = '1' then
-      PR_Cnt <= (others => '0');
-      PR_FC <= '0';
-    elsif CLK'event and CLK = '1' then
-      -- Ahora tenemos que ver si podemos seguir contando
-      if FSM_PR = '1' then
-        -- Significa que podemos contar, aunque si estamos en 27, tendremos que reiniciar
-        if PR_Cnt = tiempoMuestreo - 1 then
-          -- Se verifica que se ha llegado al fin de cuenta, y que tenemos que reiniciar
-          PR_Cnt <= (others => '0');
-          PR_FC <= '1';
-        else
-          -- No estamos a fin de cuenta, por tanto tenemos que incrementar el contador
-          PR_Cnt <= PR_Cnt + 1;
-          PR_FC <= '0';
-        end if;
-      else
-        -- Verificamos que no podemos contar porque la maquina de estados no esta recibiendo, por tanto reiniciamos
         PR_Cnt <= (others => '0');
         PR_FC <= '0';
-      end if;
-    end if;
-  end process;
-
-  process(PR_Out, FSM_CntM, CLK, RST) -- Proceso que modela el Contador que cuenta las muestras realizadas sobre un bit
-  begin
-    if RST = '1' then
-      CntM_Out <= (others => '0');
-     
-    elsif (CLK'event) and (CLK = '1') and (PR_Out = '1') then -- Ahora tenemos que comprobar si la Maquina de Estados nos deja contar o no
-      if FSM_CntM = '1' then
-        -- Podemos contar, y en su caso, reiniciar el contador
-        if CntM_Out < 15 then
-          -- Verificamos que no estamos en el fin de cuenta, por tanto incrementamos en 1 el contador
-          CntM_Out <= CntM_Out + 1;
-        elsif CntM_Out = 15 then
-          -- Verificamos que estamos en el fin de cuenta, por tanto tenemos que reiniciar el contador
-          CntM_Out <= (others => '0');
-        end if;
-      else
-        -- Se verifica que la Maquina de Estados no nos deja contar, por tanto reseteamos el contador para el siguiente dato
-        CntM_Out <= (others => '0');
-      end if;
-    end if;
-  end process;
-
-  process(CntM_Out, FSM_CntB, CLK, RST) -- Proceso que modela el Contador que cuenta los bits leidos
-  begin
-    if RST = '1' then
-      CntB_Out <= (others => '0');
-
     elsif CLK'event and CLK = '1' then
-      if FSM_CntB = '1' then
-        -- Verificamos que la maquina de estados deja contar
-        if (CntM_Out = 15) then
-          if CntB_Out < 11 then
-            CntB_Out <= CntB_Out + 1;
-          else
-            CntB_Out <= (others => '0');
-          end if;
+        if STD_Act = idle then 
+            PR_Cnt <= (others => '0');
+            PR_FC <= '0';       
+        elsif STD_Act = Receiving then
+             if PR_Cnt = tiempoMuestreo - 1 then
+                PR_Cnt <= (others=>'0');
+                PR_FC <= '1';
+             else
+                PR_Cnt <= PR_Cnt + 1;
+                PR_FC <= '0';
+             end if;
         end if;
-      else
-        -- Verificamos que la maquina de estados no nos deja contar, por tanto resetamos para el siguiente dato
-        CntB_Out <= (others => '0');
-      end if;
+    end if;
+  end process;
+    
+    
+--  process(STD_Act, PR_Out, FSM_CntM, CLK, RST) -- Proceso que modela el Contador que cuenta las muestras realizadas sobre un bit
+--  begin
+--    if RST = '1' then
+--      CntM_Cnt <= (others => '0');
+     
+--    elsif (CLK'event) and (CLK = '1') and (PR_Out = '1') then -- Ahora tenemos que comprobar si la Maquina de Estados nos deja contar o no
+--      if FSM_CntM = '1' then
+--        -- Podemos contar, y en su caso, reiniciar el contador
+--        if CntM_Out < 15 then
+--          -- Verificamos que no estamos en el fin de cuenta, por tanto incrementamos en 1 el contador
+--          CntM_Out <= CntM_Out + 1;
+--        elsif CntM_Out = 15 then
+--          -- Verificamos que estamos en el fin de cuenta, por tanto tenemos que reiniciar el contador
+--          CntM_Out <= (others => '0');
+--        end if;
+--      else
+--        -- Se verifica que la Maquina de Estados no nos deja contar, por tanto reseteamos el contador para el siguiente dato
+--        CntM_Out <= (others => '0');
+--      end if;
+--    end if;
+--  end process;
+  
+  Process(STD_Act, PR_FC, CLK, RST)
+  begin
+    if RST = '1' then
+        CntM_Cnt <= (others => '0');
+        CntM_FC <= '0';
+    elsif CLK'event and CLK = '1' then
+        if STD_Act = idle then
+            CntM_Cnt <= (others => '0');
+            CntM_FC <= '0';
+        elsif STD_Act = receiving then
+            if PR_FC = '1'then
+                if CntM_Cnt >= numeroMuestras - 1 then
+                    CntM_Cnt <= (others => '0');
+                    CntM_FC <= '1';
+                else
+                    CntM_Cnt <= CntM_Cnt + 1;
+                end if;
+             else 
+                CntM_FC <= '0';   
+            end if;
+        end if;
     end if;
   end process;
 
+--  process(STD_Act, CntM_Out, FSM_CntB, CLK, RST) -- Proceso que modela el Contador que cuenta los bits leidos
+--  begin
+--    if RST = '1' then
+--      CntB_Out <= (others => '0');
+
+--    elsif CLK'event and CLK = '1' then
+--      if FSM_CntB = '1' then
+--        -- Verificamos que la maquina de estados deja contar
+--        if (CntM_Out = 15) then
+--          if CntB_Out < 11 then
+--            CntB_Out <= CntB_Out + 1;
+--          else
+--            CntB_Out <= (others => '0');
+--          end if;
+--        end if;
+--      else
+--        -- Verificamos que la maquina de estados no nos deja contar, por tanto resetamos para el siguiente dato
+--        CntB_Out <= (others => '0');
+--      end if;
+--    end if;
+--  end process;
+  process(STD_Act, CntM_FC, CLK, RST)
+  begin
+      if RST = '1' then
+          CntB_Cnt <= (others => '0');
+          CntB_FC <= '0';
+      elsif CLK'event and CLK = '1' then
+          if STD_Act = idle then
+              CntB_Cnt <= (others => '0');
+              CntB_FC <= '0';
+          elsif STD_Act = receiving then
+              if CntM_FC = '1' then
+                  if CntB_Cnt >= numeroBits - 1 then
+                      CntB_Cnt <= (others => '0');
+                      CntB_FC <= '1';
+                  else
+                      CntB_Cnt <= CntB_Cnt + 1;
+                  end if;
+              else 
+                CntB_FC <= '0';
+              end if;
+          end if;
+      end if;
+  end process;
+   
+  
   process(RX, PR_FC, CLK, RST) -- Proceso que modela el Registro de Desplazamiento encargado del sobremuestreo de RX
   begin
     if RST = '1' then
@@ -139,13 +212,26 @@ begin
     end if;
   end process;
 
-  process(CntBin, CntM_Out, CLK, RST) -- Proceso que modela el Registro de Desplazamiento a la salida del contador de 0 y 1
+--  process(STD_Act , CntBin, CntM_Out, CLK, RST) -- Proceso que modela el Registro de Desplazamiento a la salida del contador de 0 y 1
+--  begin
+--    if RST = '1' then
+--      RDB_Out <= (others => '0');
+--    elsif (CLK'event and CLK = '1') and (CntM_Out = 15) then
+--      -- Cada vez que se completa una muestra, podemos leer la entrada
+--      RDB_Out <= CntBin & RDB_Out(10 downto 1);
+--    end if;
+--  end process;
+
+  process(STD_Act, CntBin, CLK, RST) -- Proceso que modela el Registro de Desplazamiento a la salida del contador de 0 y 1
   begin
-    if RST = '1' then
-      RDB_Out <= (others => '0');
-    elsif (CLK'event and CLK = '1') and (CntM_Out = 15) then
-      -- Cada vez que se completa una muestra, podemos leer la entrada
-      RDB_Out <= CntBin & RDB_Out(10 downto 1);
+  if RST = '1' then
+    RDB_Out <= (others => '0');
+    elsif CLK'event and CLK = '1' then
+        if STD_Act = idle then
+            RDB_Out <= (others=>'0'); 
+        elsif STD_Act = receiving then
+            RDB_Out <= CntBin & RDB_Out(10 downto 1);
+        end if;
     end if;
   end process;
 
@@ -171,17 +257,32 @@ begin
   end if;
   end process;
 
-  process(RDB_Out, FSM_Val, CLK, RST) -- Proceso que modela el Registro que almacena los datos y los envia a la salida
+--  process(STD_Act , RDB_Out, FSM_Val, CLK, RST) -- Proceso que modela el Registro que almacena los datos y los envia a la salida
+--  begin
+
+--    if RST = '1' then
+--      dato_rx <= (others => '0');
+--    elsif (CLK'event and CLK = '1') and (FSM_VAL = '1') then
+--      -- Se verifica que la Maquina de estados da la orden a que se envie el dato, por tanto, lo enviamos
+--      dato_rx <= RDB_Out(8 downto 1);
+--    end if;
+--  end process;
+    
+  process(STD_Act, RDB_Out, CLK, RST)
   begin
     if RST = '1' then
-      dato_rx <= (others => '0');
-    elsif (CLK'event and CLK = '1') and (FSM_VAL = '1') then
-      -- Se verifica que la Maquina de estados da la orden a que se envie el dato, por tanto, lo enviamos
-      dato_rx <= RDB_Out(8 downto 1);
+        dato_rx <= (others=> '0');
+    elsif CLK'event and CLK = '1' then
+        if STD_Act = idle then
+           dato_rx_ok <='0';
+        elsif STD_Act = outputing then
+            dato_rx_ok <= '1';
+            dato_rx <= RDB_Out(8 downto 1);
+        end if;
     end if;
   end process;
     
-  process(rx, CNTB_Out, Val_Out, STD_Act, CLK, RST)-- Proceso que modela la Maquina Finita de Estados, cuyos estados son: Idle, Receiving, Outputing, Verifying y Error
+  process(rx, CntB_FC, Val_Out, STD_Act, CLK, RST)-- Proceso que modela la Maquina Finita de Estados, cuyos estados son: Idle, Receiving, Outputing, Verifying y Error
   begin 
     if RST = '1' then 
       STD_Act <= Idle;
@@ -193,7 +294,7 @@ begin
             STD_Act <= Receiving;
           end if;
         when Receiving =>
-          if CntB_Out >= 11 then
+          if CntB_FC = '1' then
             STD_Act <= Verifying;
           end if;
         when Verifying =>
@@ -207,39 +308,39 @@ begin
             STD_Act <= Idle;
           end if;
         when Error =>
-          if Val_Out = '0' then
+          if Val_Out = '0' then --rarete chequear si da problemas
             STD_Act <= Idle;        
           end if;        
       end case;   
     end if;
   end process;
 
-  process(RX, STD_Act) -- Proceso que modela el calculo de las salidas de la maquina de estados en cada estado
-  begin
-    case STD_Act is
-      when Idle =>
-      FSM_PR <= '0';
-      FSM_CntM <= '0';
-      FSM_CntB <= '0';
-      FSM_Val <= '0';
-      dato_rx_ok <= '0';
-      error_recep <= '0';
-        if RX = '0' then
-          error_recep <= '0';
-        end if;
-      when Receiving =>
-        FSM_PR <= '1';
-        FSM_CntB <= '1';
-        FSM_CntM <= '1';
-      when Verifying =>
-        FSM_PR <= '0';
-        FSM_CntB <= '0';
-        FSM_CntM <= '0';
-      when Error =>
-        error_recep <= '1';
-      when Outputing =>
-        FSM_Val <= '1';
-        dato_rx_ok <= '1';
-      end case;
-  end process;
+--  process(RX, STD_Act) -- Proceso que modela el calculo de las salidas de la maquina de estados en cada estado
+--  begin
+--    case STD_Act is
+--      when Idle =>
+--      FSM_PR <= '0';
+--      FSM_CntM <= '0';
+--      FSM_CntB <= '0';
+--      FSM_Val <= '0';
+--      dato_rx_ok <= '0';
+--      error_recep <= '0';
+--        if RX = '0' then
+--          error_recep <= '0';
+--        end if;
+--      when Receiving =>
+--        FSM_PR <= '1';
+--        FSM_CntB <= '1';
+--        FSM_CntM <= '1';
+--      when Verifying =>
+--        FSM_PR <= '0';
+--        FSM_CntB <= '0';
+--        FSM_CntM <= '0';
+--      when Error =>
+--        error_recep <= '1';
+--      when Outputing =>
+--        FSM_Val <= '1';
+--        dato_rx_ok <= '1';
+--      end case;
+--  end process;
 end rtl;
